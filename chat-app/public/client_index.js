ec32bb0b03d79476204f87f3cfb2d6422edac54a
AkitoSekiya <147584706+AkitoSekiya@users.noreply.github.com>
2024-06-25 13:22:41 +0900

    Add files via upload

---
+++ chat-app/public/client_index.js
@@ -0,0 +1,17 @@
+const hostButton = document.getElementById('host');
+hostButton.style.height = (window.innerHeight * 0.2) + "px";
+hostButton.style.width = (window.innerWidth * 0.2) + "px";
+const audienceButton = document.getElementById('audience');
+audienceButton.style.height = (window.innerHeight * 0.2) + "px";
+audienceButton.style.width = (window.innerWidth * 0.2) + "px";
+
+
+audienceButton.onclick = () => {
+    window.open("audience.html", null, "top=0,left=" + (screen.width - 500) + ",width=" + "500" + ",height=" + screen.availHeight);
+};
+
+messageInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+    sendButton.click();
+    }
+});
