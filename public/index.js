38aa46b6970d9731a9c2ea3b88be3dc69f94d303
GoogleBr35 <sazai7089@gmail.com>
2024-07-02 17:37:17 +0900

    7/2 Soya

--- public/index.js
+++ public/index.js
@@ -5,12 +5,12 @@ const audienceButton = document.getElementById('audience');
 audienceButton.style.height = (window.innerHeight * 0.2) + "px";
 audienceButton.style.width = (window.innerWidth * 0.2) + "px";
 
-
+/* 「オーディエンス」ボタンを押したときの処理 */
 audienceButton.onclick = () => {
     window.open("audience.html", null, "top=0,left=" + (screen.width - 500) + ",width=" + "500" + ",height=" + screen.availHeight);
 };
 
-// 「主催者」ボタンを押したときの処理
+/* 「主催者」ボタンを押したときの処理 */
 hostButton.onclick = () => {
     var oneTimePass = prompt("ワンタイムパスワードを入力してください");
     if(true/* サーバのワンタイムパスワードと一致したら開くようにしたい...! */) window.open("host.html", null, "top=0,left=" + (screen.width - 500) + ",width=" + "500" + ",height=" + screen.availHeight);
@@ -18,6 +18,6 @@ hostButton.onclick = () => {
 
 messageInput.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
-    sendButton.click();
+        sendButton.click();
     }
 });
