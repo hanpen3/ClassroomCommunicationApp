09cf36d0b27d64d6a859866f7a7c8c2f51590476
hanpen3 <ishihara-emina-sb@ynu.jp>
2024-07-08 15:38:17 +0900

    commit audience

--- public/audience.js
+++ public/audience.js
@@ -4,9 +4,13 @@ chat.style.width = (window.innerWidth * 0.9) + "px";
 const messageInput = document.getElementById('message');
 messageInput.style.height = (window.innerHeight * 0.05) + "px";
 messageInput.style.width = (window.innerWidth * 0.7) + "px";
-const sendButton = document.getElementById('send');
-sendButton.style.height = (window.innerHeight * 0.05) + "px";
-sendButton.style.width = (window.innerWidth * 0.2) + "px";
+const commentButton = document.getElementById('comment');
+commentButton.style.height = (window.innerHeight * 0.05) + "px";
+commentButton.style.width = (window.innerWidth * 0.2) + "px";
+const questionButton = document.getElementById('question');
+questionButton.style.height = (window.innerHeight * 0.05) + "px";
+questionButton.style.width = (window.innerWidth * 0.2) + "px";
+
 
 const hostname = window.location.hostname;
 const ws = new WebSocket(`ws://${hostname}:3000`);
@@ -36,7 +40,7 @@ ws.onclose = (event) => {
     chat.scrollTop = chat.scrollHeight;
 };
 
-sendButton.onclick = () => {
+commentButton.onclick = () => {
     const message = messageInput.value;
     if (message) {
     ws.send(username + ": " + message); // 文字列として送信
@@ -46,6 +50,6 @@ sendButton.onclick = () => {
 
 messageInput.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
-    sendButton.click();
+    commentButton.click();
     }
 });
