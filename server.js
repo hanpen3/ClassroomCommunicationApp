2ef4ea7d04e43042e83a2b1d93c4d7c60fcfc251
Akito Sekiya <akito.sekiya@gmail.com>
2024-07-09 16:29:16 +0900

    a

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const wss = new WebSocket.Server({ server });
 app.use(express.static('public')); // 提供ディレクトリを'public'に設定
 
 let clients = [];
-// var num_of_connection = 0;
 var oneTimePass;
 let host;
 
@@ -30,6 +29,7 @@ wss.on('connection', (ws) => {
         const type = data.type;
         const name = data.name;
         const content = data.content;
+        var sobj; //送信するオブジェクト
 
         console.log(`MessageReceived type:${type}, name:${name}, "${content}"`);
 
@@ -44,21 +44,8 @@ wss.on('connection', (ws) => {
             }
             console.log("voteを送信しました");
             break;
-        case 'worksheet': //ワークシートの内容を受信
-            console.log('Received worksheet content: ' + data.content);
-            // ワークシートの内容を全クライアントに送信
-            wss.clients.forEach((client) => {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({
-                        type: 'worksheet',
-                        name: 'server',
-                        content: data.content
-                    }));
-                }
-            });
-            break;
         case 'passCheck':
-            const sobj = {
+            sobj = {
                 type: 'passSend',
                 name: 'server',
                 content: oneTimePass
@@ -84,7 +71,6 @@ wss.on('connection', (ws) => {
     /* クライアント切断時の動作 */
     ws.on('close', () => {
         console.log('Client disconnected');
-        
         clients = clients.filter(client => client !== ws);
     });
 });
