5bb5f6405026500b8c3c832c262b4482b8c6efa4
kurumi0505 <kurumisawa-daichi-fr@ynu.jp>
2024-07-09 16:36:40 +0900

    変更！

--- server.js
+++ server.js
@@ -44,19 +44,19 @@ wss.on('connection', (ws) => {
             }
             console.log("voteを送信しました");
             break;
-            case 'worksheet': //ワークシートの内容を受信
-            console.log('Received worksheet content: ' + data.content);
-            // ワークシートの内容を全クライアントに送信
-            wss.clients.forEach((client) => {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({
-                        type: 'worksheet',
-                        name: 'server',
-                        content: data.content
-                    }));
-                }
-            });
-            break;
+        case 'worksheet': //ワークシートの内容を受信
+        console.log('Received worksheet content: ' + data.content);
+        // ワークシートの内容を全クライアントに送信
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({
+                    type: 'worksheet',
+                    name: 'server',
+                    content: data.content
+                }));
+            }
+        });
+        break;
         case 'passCheck':
             sobj = {
                 type: 'passSend',
